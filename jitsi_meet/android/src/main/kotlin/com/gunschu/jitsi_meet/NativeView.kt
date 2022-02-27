a2a88ca2714f8b219c07b3d2f674f00ca048ac22
usamaahm61 <usamaahm61@gmail.com>
2022-02-28 03:20:11 +0500

    make meet options dynamic

--- jitsi_meet/android/src/main/kotlin/com/gunschu/jitsi_meet/NativeView.kt
+++ jitsi_meet/android/src/main/kotlin/com/gunschu/jitsi_meet/NativeView.kt
@@ -8,6 +8,8 @@ import android.view.View
 import android.widget.TextView
 import io.flutter.plugin.platform.PlatformView
 import java.net.URL
+import android.util.Log
+
 
 internal class NativeView(activity: Activity, context: Context, id: Int, creationParams: Map<String?, Any?>?) : PlatformView {
     private val jitsiView: RNJitsiMeetView
@@ -19,18 +21,34 @@ internal class NativeView(activity: Activity, context: Context, id: Int, creatio
     override fun dispose() {
         jitsiView.leave()
     }
-    
+
     init {
         val userInfo = RNJitsiMeetUserInfo()
-        userInfo.displayName = "John"
-        userInfo.email = "Doe"
+        userInfo.displayName = creationParams?.get("userDisplayName")?.toString()
+        userInfo.email = creationParams?.get("userEmail")?.toString()
+        if (creationParams?.get("userAvatarURL") != null) {
+            userInfo.avatar = URL(creationParams?.get("userAvatarURL")?.toString())
+        }
+        val optionsBuilder = RNJitsiMeetConferenceOptions.Builder()
+        val audioMuted: Boolean = true
+        val videoMuted: Boolean = true
+        val featureFlags = creationParams?.get("featureFlags")
 
-        val options = RNJitsiMeetConferenceOptions.Builder()
+        optionsBuilder
             .setServerURL(URL("https://meet.jit.si"))
-            .setRoom("abcd")
+            .setRoom(creationParams?.get("room")?.toString())
+            .setSubject(creationParams?.get("subject")?.toString())
+            .setAudioMuted(audioMuted)
+            .setVideoMuted(videoMuted)
             .setUserInfo(userInfo)
-            .build()
+            .setFeatureFlag("chat.enabled",false)
+            .setFeatureFlag("pip.enabled", false)
+            .setFeatureFlag("tile-view.enabled", true)
+            .setFeatureFlag("audio.mute.enabled", false)
+            .setFeatureFlag("overflow-menu.enabled", false)
 
+
+        val options = optionsBuilder.build()
         val view = RNJitsiMeetView(activity)
         view.join(options)
 
